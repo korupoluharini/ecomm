6d29a4093ffbcd2111320ba4442094a21c194a54
Harini <harini@Harinis-MacBook-Pro.local>
2011-11-02 15:53:44 -0500

    init

---
+++ ecomm/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecomm::Application.config.secret_token = 'cc1914ba29dc43eb81ae58e6e9310f753819ebaf2645f71ffa54dd4c119167d0c0c453f094eb6012c643321eec52c9d6adf2c43b371f2ac78ed9f1d9336c5a5b'
